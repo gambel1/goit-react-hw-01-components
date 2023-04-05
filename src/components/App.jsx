ae345b58c3f5edc5ce4cfcd02aec080dc77400e7
Алексей <electrifyx@gmail.com>
2023-04-05 12:34:39 +0300

    edit task# 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,19 @@
-// import Profile from "./Profile";
-import ProfileList from "./ProfileList";
-import Section from './Section';
+import Profile from "./Profile";
 import user from '../user.json';
 
-export default function App() {
-  return (<div>
-    <Section>
-      <ProfileList items={user}></ProfileList>
-    </Section>
-      
-
-  </div>)
+export default function ProfileList() {
+  return (
+    <div>
+      <Profile
+        ImageSrc={user.avatar}
+        alt={user.username}
+        title={user.username}
+        tag={user.tag}
+        location={user.location}
+        followers={user.followers}
+        Views={user.views}
+        Likes={user.likes}
+      />
+    </div>
+  );
 }
--- src/components/Section.js
+++
@@ -1,6 +0,0 @@
-// import PropTypes from 'prop-types';
-export default Section;
-
-function Section({ children }) {
-  return <div>{children}</div>;
-}
